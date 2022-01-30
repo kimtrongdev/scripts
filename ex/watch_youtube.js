15851a46fa084b7dedfa8d3ee7d7eaa646118721
kim <kim@gail.com>
2022-01-30 21:38:41 +0700

    [playlist] add loop mode active

--- ex/watch_youtube.js
+++ ex/watch_youtube.js
@@ -518,6 +518,13 @@ async function preWatchingVideo(action){
             await userClick(action.pid,'', randomVideoModeBtn)
             await sleep(1000)
         }
+
+        let loopModeBtn = document.querySelector('.style-grey-text #button > yt-icon > svg > g > path[d="M21,13h1v5L3.93,18.03l2.62,2.62l-0.71,0.71L1.99,17.5l3.85-3.85l0.71,0.71l-2.67,2.67L21,17V13z M3,7l17.12-0.03 l-2.67,2.67l0.71,0.71l3.85-3.85l-3.85-3.85l-0.71,0.71l2.62,2.62L2,6v5h1V7z"]')
+        if (loopModeBtn) {
+            await userClick(action.pid,'', loopModeBtn)
+            await sleep(1000)
+        }
+        
         action.playlist_index = action.playlist_index == undefined ? (action.total_times + randomRanger(0, 1)) : action.playlist_index
         console.log('playlist_index:', action.playlist_index)
         action.playlist_index = action.playlist_index - 1
