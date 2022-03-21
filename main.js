ed790404107c2611ceb14dd2da120b457fa1e24c
kim <kim@gail.com>
2022-03-21 22:33:42 +0700

    fix main

--- main.js
+++ main.js
@@ -717,7 +717,7 @@ function initExpress() {
 
         if (req.query.isScriptReport) {
             closeChrome(req.query.pid)
-            request_api.reportScript(action.pid, req.query.service_id)
+            request_api.reportScript(req.query.pid, req.query.service_id)
         }
         else if (req.query.id == 'channel-position') {
             let channel = usersPosition.find(u => u.pid == req.query.pid)
