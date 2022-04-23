ee6b8422b4636ca84f1fb1acb615b8af15275bd5
kim <kim@gail.com>
2022-04-23 22:21:03 +0700

    revert trace

--- trace/js/background/prefs.js
+++ trace/js/background/prefs.js
@@ -404,7 +404,7 @@ var Prefs = {
 			"enabled":true,
 			"uaOSConfig":{
 				"AllowMac":{
-					"enabled":true
+					"enabled":false
 				},
 				"AllowLinux":{
 					"enabled":false
@@ -415,7 +415,7 @@ var Prefs = {
 			},
 			"uaWBConfig":{
 				"AllowChrome":{
-					"enabled":true
+					"enabled":false
 				},
 				"AllowFirefox":{
 					"enabled":false
@@ -427,7 +427,7 @@ var Prefs = {
 					"enabled":false
 				},
 				"AllowEdge":{
-					"enabled":false
+					"enabled":true
 				},
 				"AllowSafari":{
 					"enabled":false
--- trace/js/background/vars.js
+++ trace/js/background/vars.js
@@ -15,8 +15,8 @@ var Vars = {
 	pauseEnd:0,
 
 	// Refresh constants
-	UserAgentInterval:1,
-	GPUInterval:1,
+	UserAgentInterval:999999999,
+	GPUInterval:999999999,
 	FakeIPInterval:1,
 
 	uninstallUrl:"https://absolutedouble.co.uk/trace/extension-uninstall?e=",
@@ -46,19 +46,21 @@ var Vars = {
 		"os": {
 			"windows":{
 				"Windows 10 (x64)": "Windows NT 10.0; Win64; x64",
-				"Windows 10 (x86)": "Windows NT 10.0; en-US",
+			//	"Windows 10 (x86)": "Windows NT 10.0; en-US",
 				"Windows 8.1 (x64)":"Windows NT 6.3; Win64; x64",
-				"Windows 8.1 (x86)":"Windows NT 6.3; en-US",
+			//	"Windows 8.1 (x86)":"Windows NT 6.3; en-US",
 				"Windows 8 (x64)":"Windows NT 6.2; Win64; x64",
-				"Windows 8 (x86)":"Windows NT 6.2; en-US",
+			//	"Windows 8 (x86)":"Windows NT 6.2; en-US",
 				"Windows 7 (x64)":"Windows NT 6.1; Win64; x64",
-				"Windows 7 (x86)":"Windows NT 6.1; en-US"
+			//	"Windows 7 (x86)":"Windows NT 6.1; en-US"
 			},
 			"linux":{
 				"linux 64bit":"X11; Linux x86_64",
-				"linux 32bit":"X11; Linux x86_32"
+				//"linux 32bit":"X11; Linux x86_32",
+				//"linux 64bit":"X11; Linux armv7l", 
 			},
 			"macos":{
+                "macos monteray":"Macintosh; Intel Mac OS X 10_15_7",
 				"macos mojave3":"Macintosh; Intel Mac OS X 10_14_6",
 				"macos mojave2":"Macintosh; Intel Mac OS X 10_14_0",
 				"macos mojave":"Macintosh; Intel Mac OS X 10_14",
@@ -69,48 +71,57 @@ var Vars = {
 		},
 		"wb":{
 			"chrome":{
-				"86":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+				//"86":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"dsfdsf
+				"100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+				"100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+				//"99":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 Safari/537.36",
+				"101":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4893.203 Safari/537.36",
+				//"101":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.34 Safari/537.36",
 			},
 			"firefox":{
 				"82":"Gecko/20100101 Firefox/82.0",
 				"81":"Gecko/20100101 Firefox/81.0"
 			},
 			"vivaldi":{
-				"2.70":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.137 Safari/537.36 Vivaldi/2.7.1628.33",
-				"2.30":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.82 Safari/537.36 Vivaldi/2.3.1440.41"
+				//"84":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.23",
+				//"2.70":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.137 Safari/537.36 Vivaldi/2.7.1628.33",
+				//"2.30":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.82 Safari/537.36 Vivaldi/2.3.1440.41"
 			},
 			"opera":{
-				"57":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106",
-				"54":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.54"
+				"86":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.23",
+				"80":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4874.167 Safari/537.36 OPR/80.0.3067.72",
+				"85":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.65",
+
+				//"46":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36 OPR/46.0.2207.0 OMI/4.20.3.54.Catcher2.202 HbbTV/1.5.1 (+DRM;Hisense;SmartTV_2020;V0000.01.00S.M0222;HE65U6F1UWTSG;SmartTV_2020;) FVC/4.0 (Hisense;SmartTV_2020;) LaTivu_1.0.1_2020",
+				//"57":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106",
+				//"54":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.54"
 			},
 			"edge":{
-				"cr88":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4287.0 Safari/537.36 Edg/88.0.673.0",
-				"cr86":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.56"
+				"cr100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4664.110 Safari/537.36 Edg/100.0.1054.62",
+				"cr100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4855.0 Safari/537.36 Edg/100.0.1160.0",
+				"cr100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50",
+				"cr100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.44",
+				"cr101":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.34 Safari/537.36 Edg/101.0.1210.19",
+				"cr100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36 Edg/100.0.1185.88"
+
+
+				//"cr88":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4287.0 Safari/537.36 Edg/88.0.673.0",
+				//"cr86":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.56"
 			},
 			"safari":{
-				"12.1":"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15",
-				"12.0":"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15",
+				"100":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+				//"12.1":"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15",
+				//"12.0":"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15",
 			}
 		}
 	},
 
 	gpuModels: [
-		'AMD Radeon HD 6350',
-		'AMD Radeon HD 6450',
-		'AMD Radeon HD 6800 Series',
-		'AMD Radeon HD 7310 Graphics',
-		'AMD Radeon HD 7340 Graphics',
-		'AMD Radeon HD 7520G',
-		'AMD Radeon HD 7640G',
+	/*	
 		'AMD Radeon HD 7700 Series',
 		'AMD Radeon HD 7800 Series',
 		'AMD Radeon HD 8240',
-		'AMD Radeon R7 200 Series',
-		'AMD Radeon R7 300 Series',
-		'AMD Radeon R9 200 Series',
-		'AMD Radeon R9 300 Series',
-		'AMD Radeon R9 400 Series',
-
+		
 		// AMD 5000 series
 		'AMD Radeon RX 5500 XT',
 		'AMD Radeon RX 5600 XT',
@@ -131,24 +142,7 @@ var Vars = {
 		'Intel(R) HD Graphics',
 		'Intel(R) HD Graphics Family',
 
-		// 2nd Gen Intel processors
-		'Intel(R) HD Graphics 2000',
-		'Intel(R) HD Graphics 3000',
-
-		// 3rd Gen Intel processors
-		'Intel(R) HD Graphics 2500',
-		'Intel(R) HD Graphics 4000',
-
-		// 4th Gen Intel processors
-		'Intel(R) HD Graphics 4200',
-		'Intel(R) HD Graphics 4400',
-		'Intel(R) HD Graphics 4600',
-		'Intel(R) HD Graphics 5000',
-
-		// 5th Gen Intel processors
-		'Intel(R) HD Graphics 5300',
-		'Intel(R) HD Graphics 5500',
-		'Intel(R) HD Graphics 6000',
+	,
 		'Intel(R) HD Graphics 6100',
 		'Intel(R) HD Graphics 6200',
 
@@ -181,25 +175,6 @@ var Vars = {
 		'Intel(R) Graphics Media Accelerator 3600 Series',
 		'Intel(R) Graphics Media Accelerator HD',
 
-		'NVIDIA GeForce 8400 GS',
-		'NVIDIA GeForce 9200',
-		'NVIDIA GeForce 9500 GT',
-		'NVIDIA GeForce 9800 GT',
-		'NVIDIA GeForce GT 220',
-		'NVIDIA GeForce GT 240',
-		'NVIDIA GeForce GT 430',
-		'NVIDIA GeForce GT 440',
-		'NVIDIA GeForce GT 610',
-		'NVIDIA GeForce GT 620',
-		'NVIDIA GeForce GT 630',
-		'NVIDIA GeForce GT 640',
-		'NVIDIA GeForce GTX 550 Ti',
-		'NVIDIA GeForce GTX 560',
-		'NVIDIA GeForce GTX 560 Ti',
-		'NVIDIA GeForce GTX 650',
-		'NVIDIA GeForce GTX 660',
-		'NVIDIA GeForce GTX 670',
-		'NVIDIA GeForce GTX 680',
 		'NVIDIA GeForce GTX 760',
 		'NVIDIA Quadro 4000M',
 		'NVIDIA Quadro 2000M',
@@ -214,7 +189,55 @@ var Vars = {
 		'NVIDIA GeForce GTX 1050M',
 		'NVIDIA GeForce GTX 1060M',
 		'NVIDIA GeForce GTX 1070M',
-		'NVIDIA GeForce GTX 1080M'
+		'NVIDIA GeForce GTX 1080M',
+		*/
+	
+		//
+		'NVIDIA, NVIDIA GeForce GTX 1050' ,
+		'NVIDIA, NVIDIA GeForce GTX 1660 SUPER',
+		'NVIDIA, NVIDIA GeForce GTX 970',
+		'NVIDIA, NVIDIA GeForce RTX 3060 Laptop GPU',
+		'NVIDIA, NVIDIA GeForce GT 620',
+		'NVIDIA, NVIDIA GeForce GTX 1080',
+		'NVIDIA, NVIDIA GeForce GT 1030',
+		'NVIDIA GeForce GTX 1050 Ti',
+		'NVIDIA, NVIDIA GeForce GTX 1080 Ti',
+		'NVIDIA, NVIDIA GeForce GTX 960',
+		'NVIDIA, NVIDIA GeForce GTX 1060 3GB',
+		'NVIDIA, NVIDIA GeForce GTX 1660 Ti',
+		'NVIDIA, NVIDIA GeForce GTX 1060',
+		'NVIDIA, NVIDIA GeForce RTX 3060',
+		'NVIDIA GeForce GTX 1060 3GB',
+		'NVIDIA GeForce RTX 2060',
+		'NVIDIA, NVIDIA GeForce GT 720M',
+		'NVIDIA, NVIDIA GeForce GTX 1050 Ti',
+		'NVIDIA, NVIDIA GeForce GTX 1650',
+		'NVIDIA, NVIDIA GeForce RTX 3050 Ti Laptop GPU',
+		'NVIDIA GeForce GTX 1060 6GB',
+		'NVIDIA, NVIDIA GeForce GTX 1650 Ti',
+		'NVIDIA GeForce 920MX',
+		'NVIDIA, NVIDIA GeForce GTX 950M',
+		'NVIDIA, NVIDIA GeForce RTX 2080 SUPER',
+		'NVIDIA GeForce RTX 3060 Ti',
+		'NVIDIA, NVIDIA GeForce RTX 2060',
+		'NVIDIA, NVIDIA GeForce RTX 3070',
+		'NVIDIA, NVIDIA GRID K280Q',
+		'NVIDIA, NVIDIA GeForce GTX 1660',
+		'NVIDIA, NVIDIA GeForce RTX 2070 SUPER',
+		'NVIDIA, NVIDIA Quadro RTX 4000',
+		'NVIDIA GeForce GTX 1650',
+		'NVIDIA GeForce RTX 3060 Laptop GPU',
+		'NVIDIA, NVIDIA GeForce GTX 1060 6GB',
+		'NVIDIA, NVIDIA GeForce GTX 750 Ti',
+		'NVIDIA, NVIDIA GeForce GT 730',
+		'NVIDIA, NVIDIA GeForce GTX 1070',
+		'NVIDIA GeForce GT 710',
+		'NVIDIA, NVIDIA GeForce RTX 2060 SUPER',
+		'NVIDIA GeForce RTX 3050',
+		'NVIDIA, NVIDIA GeForce RTX 3050',
+,
+
+	
 	],
 	gpuChose:"Intel(R) HD Graphics",
 
