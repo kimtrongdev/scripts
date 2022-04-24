17bdc078f3b8662210598d91e265ba440a856dcf
kim@gmail.com <kim@gmail.com>
2022-04-24 19:20:27 +0700

    Ok

--- trace/js/background/prefs.js
+++ trace/js/background/prefs.js
@@ -404,7 +404,7 @@ var Prefs = {
 			"enabled":true,
 			"uaOSConfig":{
 				"AllowMac":{
-					"enabled":true
+					"enabled":false
 				},
 				"AllowLinux":{
 					"enabled":false
@@ -430,7 +430,7 @@ var Prefs = {
 					"enabled":true
 				},
 				"AllowSafari":{
-					"enabled":true
+					"enabled":false
 				}
 			},
 			"uaCust":{
