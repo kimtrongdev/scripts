847ec89f0ce79b474becf3e44aba685a00a75f8f
kim <kim@gail.com>
2022-05-25 11:55:29 +0700

    update for change of brave

--- ex/scripts/brave.js
+++ ex/scripts/brave.js
@@ -37,16 +37,16 @@ async function scriptCheckBat(action) {
 async function handleBraveSetting (action) {
   await updateUserInput(action.pid,'NEW_TAB', 0,0,0,0,"",'New TAB')
   await goToLocation(action.pid, `brave://settings/shields`)
-  await sleep(2000)
+  await sleep(10000)
   if (action.is_show_ui) {
     //click Fingerprinting blocking
-    await updateUserInput(action.pid,'CLICK', 755, 722,0,0,"",'click')
+    await updateUserInput(action.pid,'CLICK', 743, 788,0,0,"",'click')
     // select option
-    await updateUserInput(action.pid,'CLICK', 743,752,0,0,"",'click')
+    await updateUserInput(action.pid,'CLICK', 708, 815,0,0,"",'click')
     // click Trackers & ads blocking
-    await updateUserInput(action.pid,'CLICK', 742,580,0,0,"",'click')
+    await updateUserInput(action.pid,'CLICK', 747, 641,0,0,"",'click')
     // select option
-    await updateUserInput(action.pid,'CLICK', 719,641,0,0,"",'click')
+    await updateUserInput(action.pid,'CLICK', 693, 705,0,0,"",'click')
   } else {
     //click Fingerprinting blocking
     await updateUserInput(action.pid,'CLICK', 755, 692,0,0,"",'click')
