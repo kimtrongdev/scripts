c3b59ba2e44666546765e8be297f86a1f379369e
kim <kim@gail.com>
2022-07-01 15:47:10 +0700

    fff

--- ex/scripts/add_video_playlist.js
+++ ex/scripts/add_video_playlist.js
@@ -153,7 +153,7 @@ async function handleSearchAddVideo (action) {
       await sleep(1000)
       await userClick(action.pid, 'ytd-menu-service-item-renderer path[d="M22,13h-4v4h-2v-4h-4v-2h4V7h2v4h4V13z M14,7H2v1h12V7z M2,12h8v-1H2V12z M2,16h8v-1H2V16z"]')
       
-      let playlistItem = getElementContainsInnerText('yt-formatted-string', action.playlist_name)
+      let playlistItem = document.querySelector(`yt-formatted-string[title="${action.playlist_name}"]`)
       if (playlistItem) {
         await userClick(action.pid, '', playlistItem)
       }
