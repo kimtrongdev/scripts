b4c9c2928d8816950f75de511520f358a68f235b
kim <kim@gail.com>
2022-08-22 14:16:17 +0700

    ff

--- main.js
+++ main.js
@@ -275,8 +275,8 @@ function getProfileIds() {
 async function startChromeAction(action, _browser) {
     let widthSizes = [950, 1100, 1200]
     let positionSize = action.isNew ? 0 : utils.getRndInteger(0, 2)
-    let screenWidth = widthSizes[positionSize]
-    let screenHeight = 950 //action.isNew ? 950 : utils.getRndInteger(950, 1000)
+    let screenWidth = 1600//widthSizes[positionSize]
+    let screenHeight = 1200 //action.isNew ? 950 : utils.getRndInteger(950, 1000)
 
     //handle userDataDir
     let userDataDir =  ` --user-data-dir="${path.resolve("profiles", action.pid + '')}"`
@@ -1339,8 +1339,14 @@ async function handleAction (actionData) {
         let yStart = 150
         if (process.env.OS.indexOf('centos') > -1) {
             x = 680
-            y = 118
-            yStart = 170
+
+            if (IS_SHOW_UI) {
+                y = 118
+                yStart = 170
+            } else {
+                y = 105
+                yStart = 130
+            }
         }
         execSync(`xdotool mousemove ${x} ${y};sleep 0.5;xdotool click 1;sleep 1;xdotool mousemove ${x} ${yStart + 24 * devicePo};sleep 0.5;xdotool click 1;sleep 1`)
     }
