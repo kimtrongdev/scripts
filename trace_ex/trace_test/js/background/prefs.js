98156be7f3cbe0c304ee17b81bb539506613bb69
kim <kim@gail.com>
2022-11-05 17:40:57 +0700

    dissable Pref_ScreenRes

--- trace_ex/trace_test/js/background/prefs.js
+++ trace_ex/trace_test/js/background/prefs.js
@@ -353,13 +353,13 @@ var Prefs = {
 			"enabled":true
 		},
 		"Pref_ScreenRes":{
-			"enabled":true,
+			"enabled":false,
 			"randomOpts":{
 				"enabled":false,
 				"values":[-50,50]
 			},
 			"commonResolutions":{
-				"enabled":true,
+				"enabled":false,
 				"resolutions":[
 					[1920,1080,24],
 					[1920,1280,24],
