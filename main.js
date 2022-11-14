870a2bd2cc8cb766d5d1702ff40af9540409bb5b
kim <kim@gail.com>
2022-11-14 20:37:58 +0700

    tabs

--- main.js
+++ main.js
@@ -1685,18 +1685,18 @@ function closeChrome(pid) {
 function startDisplay(pid) {
     try {
         if (!WIN_ENV) {
-            exec(`Xvfb :${pid} -ac -screen 0, 1920x1040x24`)
-            // execSync(`unzip -o -P Trung@123456 ex.zip`)
-            // execSync(`unzip -o -P Trung@123456 quality.zip`)
-            let core = (pid % 4 + 1) * 2
-            let ram = core * (pid % 2 + 1) * 2
-            execSync(`sed -i '241 s/"value":.*/"value":${core}/' trace/js/background/prefs.js;sed -i '245 s/"value":.*/"value":${ram}/' trace/js/background/prefs.js`)
-            if (pid % 10 < 0) {
-                execSync(`sed -i '404 s/"enabled":.*/"enabled":false,/' trace/js/background/prefs.js`)
-            }
-            else {
-                execSync(`sed -i '404 s/"enabled":.*/"enabled":true,/' trace/js/background/prefs.js`)
-            }
+            // exec(`Xvfb :${pid} -ac -screen 0, 1920x1040x24`)
+            // // execSync(`unzip -o -P Trung@123456 ex.zip`)
+            // // execSync(`unzip -o -P Trung@123456 quality.zip`)
+            // let core = (pid % 4 + 1) * 2
+            // let ram = core * (pid % 2 + 1) * 2
+            // execSync(`sed -i '241 s/"value":.*/"value":${core}/' trace/js/background/prefs.js;sed -i '245 s/"value":.*/"value":${ram}/' trace/js/background/prefs.js`)
+            // if (pid % 10 < 0) {
+            //     execSync(`sed -i '404 s/"enabled":.*/"enabled":false,/' trace/js/background/prefs.js`)
+            // }
+            // else {
+            //     execSync(`sed -i '404 s/"enabled":.*/"enabled":true,/' trace/js/background/prefs.js`)
+            // }
         }
     }
     catch (e) {
@@ -1705,9 +1705,9 @@ function startDisplay(pid) {
 
 function stopDisplay(pid) {
     try {
-        if (!WIN_ENV) {
-            execSync(`pkill -f "Xvfb :${pid}"`)
-        }
+        // if (!WIN_ENV) {
+        //     execSync(`pkill -f "Xvfb :${pid}"`)
+        // }
     }
     catch (e) {
     }
@@ -1716,14 +1716,14 @@ function stopDisplay(pid) {
 function setDisplay(pid) {
     try {
         if (IS_SHOW_UI) {
-            if (MAX_CURRENT_ACC > 1) {
-                let browser = getBrowserOfProfile(pid)
-                execSync(`wmctrl -x -a ${browser}`)
-            }
+            // if (MAX_CURRENT_ACC > 1) {
+            //     let browser = getBrowserOfProfile(pid)
+            //     execSync(`wmctrl -x -a ${browser}`)
+            // }
         } else {
-            if (!WIN_ENV) {
-                process.env.DISPLAY = ':' + pid
-            }
+            // if (!WIN_ENV) {
+            //     process.env.DISPLAY = ':' + pid
+            // }
         }
     }
     catch (e) {}
