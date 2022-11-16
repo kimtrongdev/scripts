414b9d2522912f6584d7043b3ad3ac9b7ac9a3ed
kim <kim@gail.com>
2022-11-16 14:03:22 +0700

    add total_added_from_channel field

--- ex/scripts/create_playlist.js
+++ ex/scripts/create_playlist.js
@@ -136,7 +136,7 @@ async function createPlaylistScript(action) {
     else if (url.indexOf('https://www.youtube.com/channel/') > -1 || url.indexOf('https://www.youtube.com/c/') > -1 || url.indexOf('https://www.youtube.com/user/') > -1) {
       await sleep(5000)  
       let videos = document.querySelectorAll('#items #details #menu svg path[d="M12,16.5c0.83,0,1.5,0.67,1.5,1.5s-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S11.17,16.5,12,16.5z M10.5,12 c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5s-0.67-1.5-1.5-1.5S10.5,11.17,10.5,12z M10.5,6c0,0.83,0.67,1.5,1.5,1.5 s1.5-0.67,1.5-1.5S12.83,4.5,12,4.5S10.5,5.17,10.5,6z"]')
-      let total = Math.min(videos.length, 5)
+      let total = Math.min(videos.length, Number(action.total_added_from_channel) || 5)
       let count = 0
       while (count < total) {
         if (count % 5 == 0) {
