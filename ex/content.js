99f534d53dda3a7d45868b1c967ae30ef124e7fa
kim <kim@gail.com>
2022-12-02 16:31:21 +0700

    test2

--- ex/content.js
+++ ex/content.js
@@ -141,7 +141,7 @@ function initSettingData (action) {
     if (action.os_vm == 'vps') {
         //heightCustom = 23
     }
-    heightCustom = 23
+    heightCustom = -23
 
     if (action.browser_name == 'chromium-browser') {
         //heightCustom = 13
