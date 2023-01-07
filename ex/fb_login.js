63c02aef03459abe6c48fa6284621856a06b76d0
kim <kim@gail.com>
2023-01-07 20:29:52 +0700

    fb login

--- ex/fb_login.js
+++ ex/fb_login.js
@@ -4,6 +4,7 @@ async function fbLogin(action) {
     reportLive(action.pid)
 
     let url = window.location.toString()
+    url = url.split('?')[0]
 
     if (url == 'https://www.facebook.com/') {
       await updateActionStatus(action.pid, action.id, LOGIN_STATUS.SUCCESS)
@@ -11,7 +12,7 @@ async function fbLogin(action) {
       await userType(action.pid, 'input[name="email"]', action.email)
       await userTypeEnter(action.pid, 'input[name="pass"]', action.password)
     } else {
-      await updateActionStatus(action.pid, action.id, LOGIN_STATUS.ERROR, url)
+      await updateActionStatus(action.pid, action.id, LOGIN_STATUS.ERROR, url.split('?')[0])
     }
   } catch (er) {
     console.log(er);
