8ee5f9893c7e5b667e731e0bac59ff001d039e62
kim <kim@gail.com>
2023-01-28 21:34:40 +0700

    dsf

--- ex/scripts/create_playlist.js
+++ ex/scripts/create_playlist.js
@@ -26,10 +26,10 @@ async function createPlaylistScript(action) {
     if (url == 'https://www.youtube.com/') {
       if (action.channel_position == 0 || !action.channel_position) {
         await sleep(5000)
-        let checkCreateChannel1 = await getElementContainsInnerText('yt-formatted-string', 'CREATE CHANNEL')
-        let checkCreateChannel2 = await getElementContainsInnerText('yt-formatted-string', 'TẠO KÊNH')
-        let checkCreateChannel3 = await getElementContainsInnerText('yt-formatted-string', 'চ্যানেল তৈরি করুন')
-        let checkCreateChannel5 = await getElementContainsInnerText('span', 'สร้างช่อง')
+        let checkCreateChannel1 = getElementContainsInnerText('yt-formatted-string', 'CREATE CHANNEL')
+        let checkCreateChannel2 = getElementContainsInnerText('yt-formatted-string', 'TẠO KÊNH')
+        let checkCreateChannel3 = getElementContainsInnerText('yt-formatted-string', 'চ্যানেল তৈরি করুন')
+        let checkCreateChannel5 = getElementContainsInnerText('yt-button-shape', 'สร้างช่อง')
         let checkCreateChannel4 = document.querySelector('#create-channel-button tp-yt-paper-button .style-blue-text')
         let checkCreateChannel = checkCreateChannel1 || checkCreateChannel2 || checkCreateChannel3 || checkCreateChannel4 || checkCreateChannel5
         if (checkCreateChannel) {
