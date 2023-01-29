19c3b19fc7e0052dff5c69937d725e7d5c2ff5a7
kim <kim@gail.com>
2023-01-29 15:34:35 +0700

    d

--- main.js
+++ main.js
@@ -380,7 +380,7 @@ async function startChromeAction(action, _browser) {
         action.isRunBAT = isRunBAT
     }
 
-    let exs = ["ex", "quality"]
+    let exs = ["ex", "quality", "extensions/canvas", "extensions/audio", "extensions/font", "extensions/webgl"]
     let level_name = ''
     if (action.id != 'reg_user' && systemConfig.trace_names_ex.length) {
         let traceName = 'trace'
