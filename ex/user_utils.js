60ad9a547a89da52e30996d0f2872aa7a04f2839
kim <kim@gail.com>
2023-03-20 17:06:44 +0700

    refactor getElementContainsInnerText

--- ex/user_utils.js
+++ ex/user_utils.js
@@ -267,21 +267,45 @@ async function randomFullName () {
     return rs.name
 }
 
-function getElementContainsInnerText(tagName, innerText) {
-    let headings = document.evaluate(
-        `//${tagName}[contains(., '${innerText}')]`,
-        document,
-        null,
-        XPathResult.ANY_TYPE,
-        null
-    );
-
-    if (headings) {
-        const thisHeading = headings.iterateNext();
-        return thisHeading
+function getElementContainsInnerText(tagName, innerText, _document = null, type = 'contains', getType = 'fisrt') {
+    function getXPath (tagName, text) {
+      let xpathMap = {
+        'contains': `//${tagName}[contains(., '${text}')]`,
+        'equal': `//${tagName}[text()='${text}']`
+      }
+      return  xpathMap[type]
     }
+  
+    if (!Array.isArray(innerText)) {
+        innerText = [innerText]
+    }
+  
+    for (let text of innerText) {
+        let headings = document.evaluate(
+          getXPath(tagName, text),
+          _document || document,
+          null, XPathResult.ANY_TYPE, null
+        );
+        if (headings) {
+          let els = []
+          let rs = headings.iterateNext()
+          while (rs) {
+            els.push(rs)
+            rs = headings.iterateNext()
+          }
+  
+          if (els.length) {
+            if (getType == 'fisrt') {
+              return els.shift()
+            } else {
+              return els
+            }
+          }
+        }
+    }
+    
     return null
-}
+  }  
 
 async function handleSelectExOption (action) {
     if (action.trace_name && action.trace_name.indexOf('level_') > -1) {
