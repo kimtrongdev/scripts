55276639e6f1a8fbf0405589f79e63cff0239164
kim <kim@gail.com>
2023-04-07 11:53:42 +0700

    lang

--- ex/user_login.js
+++ ex/user_login.js
@@ -131,10 +131,10 @@ async function userLogin(action) {
             await sleep(30000)
         }
         else if (url.indexOf('myaccount.google.com/language') > -1) {
-            if (action.allow_verify) {
-                await updateActionStatus(action.pid, action.id, LOGIN_STATUS.ERROR, "verify_ok")
-                return
-            }
+            // if (action.allow_verify) {
+            //     await updateActionStatus(action.pid, action.id, LOGIN_STATUS.ERROR, "verify_ok")
+            //     return
+            // }
 
             if (!getElementContainsInnerText('div', 'Preferred Language')) {
                 await userClick(action.pid, 'path[d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L3 16.82V21h4.18L20.41 7.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"]')
