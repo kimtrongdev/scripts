5ab9cba15fff0a81d1cd9067e165237fe497ea78
kim <kim@gail.com>
2023-05-14 21:06:31 +0700

    add util

---
+++ ex/utils.js
@@ -0,0 +1,41 @@
+
+
+function getElementContainsInnerText(tagName, innerText, _document = null, type = 'contains', getType = 'fisrt') {
+  function getXPath (tagName, text) {
+    let xpathMap = {
+      'contains': `//${tagName}[contains(., '${text}')]`,
+      'equal': `//${tagName}[text()='${text}']`
+    }
+    return  xpathMap[type]
+  }
+
+  if (!Array.isArray(innerText)) {
+      innerText = [innerText]
+  }
+
+  for (let text of innerText) {
+      let headings = document.evaluate(
+        getXPath(tagName, text),
+        _document || document,
+        null, XPathResult.ANY_TYPE, null
+      );
+      if (headings) {
+        let els = []
+        let rs = headings.iterateNext()
+        while (rs) {
+          els.push(rs)
+          rs = headings.iterateNext()
+        }
+
+        if (els.length) {
+          if (getType == 'fisrt') {
+            return els.shift()
+          } else {
+            return els
+          }
+        }
+      }
+  }
+  
+  return null
+}
