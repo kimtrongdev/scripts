50b084f3afa163b30c405bb82cd2116261c2b6cb
kim <kim@gail.com>
2023-05-17 21:34:10 +0700

    dsf

--- ex/fb_scripts/comment_post.js
+++ ex/fb_scripts/comment_post.js
@@ -8,40 +8,27 @@ async function commentPost(action) {
 
     await checkErrorFB(action)
 
-    if (!action.selected_page && url.includes('facebook.com/pages')) {
-      await selectFBPage(action, action.post_link)
-    }
-    else if (!action.selected_page) {
-      await goToLocation(action.pid, 'https://www.facebook.com/pages/?category=your_pages')
-    }
-    else if (action.after_selected_page && url.includes('https://www.facebook.com/profile')) {
-      action.after_selected_page = false
-      await setActionData(action)
-      await goToLocation(action.pid, action.post_link)
-    }
-    else {
-      await updateUserInput(action.pid,'ESC', 0,0,0,0,"",'ESC')
-      let textboxs = document.querySelector('div[role="textbox"]')
-      let rdPos = randomRanger(0, Math.min(10, textboxs.length - 1))
-      let textbox = textboxs.item(rdPos)
+    await updateUserInput(action.pid,'ESC', 0,0,0,0,"",'ESC')
+    let textboxs = document.querySelector('div[role="textbox"]')
+    let rdPos = randomRanger(0, Math.min(10, textboxs.length - 1))
+    let textbox = textboxs.item(rdPos)
 
-      if (textbox && action.comment) {
-        await userTypeEnter(action.pid, 'textbox', action.comment, textbox)
+    if (textbox && action.comment) {
+      await userTypeEnter(action.pid, 'textbox', action.comment, textbox)
 
-        let likeData = document.querySelector('div>div>span>div>span[dir="auto"]')
-        if (likeData) {
-          let data_reported = likeData.innerText
-          action.data_reported = data_reported
-        }
-
-        await sleep(7000)
-        await checkErrorAfterRunScript(action)
-      } else {
-        console.log('NOT found text box or comment');
+      let likeData = document.querySelector('div>div>span>div>span[dir="auto"]')
+      if (likeData) {
+        let data_reported = likeData.innerText
+        action.data_reported = data_reported
       }
 
-      await reportScript(action)
+      await sleep(7000)
+      await checkErrorAfterRunScript(action)
+    } else {
+      console.log('NOT found text box or comment');
     }
+
+    await reportScript(action)
   } catch (er) {
     console.log(er);
     await reportScript(action, false)
