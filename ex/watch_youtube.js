52d6da7c3cded5a0d195ed4d87629c5bcffd4894
kim <kim@gail.com>
2023-05-22 23:07:25 +0700

    sdf

--- ex/watch_youtube.js
+++ ex/watch_youtube.js
@@ -433,6 +433,19 @@ async function watchingVideo(action){
         simpleSendKey('77')
     }
 
+    await sleep(2000)
+    let randomVideoModeBtn = document.querySelector('#top-level-buttons-computed yt-button-shape .yt-spec-button-shape-next path[d="M21,13h1v5L3.93,18.03l2.62,2.62l-0.71,0.71L1.99,17.5l3.85-3.85l0.71,0.71l-2.67,2.67L21,17V13z M3,7l17.12-0.03 l-2.67,2.67l0.71,0.71l3.85-3.85l-3.85-3.85l-0.71,0.71l2.62,2.62L2,6v5h1V7z"]')
+    if (randomVideoModeBtn) {
+        console.log('randomVideoModeBtn');
+        await userClick(action.pid,'randomVideoModeBtn', randomVideoModeBtn)
+    }
+
+    let loopModeBtn = document.querySelector('#top-level-buttons-computed yt-button-shape .yt-spec-button-shape-next path[d="M18.15,13.65l3.85,3.85l-3.85,3.85l-0.71-0.71L20.09,18H19c-2.84,0-5.53-1.23-7.39-3.38l0.76-0.65 C14.03,15.89,16.45,17,19,17h1.09l-2.65-2.65L18.15,13.65z M19,7h1.09l-2.65,2.65l0.71,0.71l3.85-3.85l-3.85-3.85l-0.71,0.71 L20.09,6H19c-3.58,0-6.86,1.95-8.57,5.09l-0.73,1.34C8.16,15.25,5.21,17,2,17v1c3.58,0,6.86-1.95,8.57-5.09l0.73-1.34 C12.84,8.75,15.79,7,19,7z M8.59,9.98l0.75-0.66C7.49,7.21,4.81,6,2,6v1C4.52,7,6.92,8.09,8.59,9.98z"]')
+    if (loopModeBtn) {
+        console.log('loopModeBtn');
+        await userClick(action.pid,'loopModeBtn', loopModeBtn)
+    }
+
     for(let i = 0; i < action.watch_time;){
         let currentUrl = window.location.toString()
         // check current url
