f4c2f5b98148e971d37318a2895d8a265f71c161
kim <kim@gail.com>
2023-05-29 20:17:21 +0700

    sdf

--- ex/fb_scripts/fb_add_member.js
+++ ex/fb_scripts/fb_add_member.js
@@ -8,14 +8,26 @@ async function fbAddMember(action) {
     await checkErrorFB(action)
 
     if (url.includes('facebook.com/groups')) {
+      let joinBtn = getElementContainsInnerText('span', ['Tham gia nhóm'], '', 'equal')
+      if (joinBtn) {
+        await userClick(action.pid, 'joinBtn', joinBtn)
+        await sleep(3000)
+      }
+
       let inviteBtn = getElementContainsInnerText('span', ['Mời'], '', 'equal')
       if (inviteBtn) {
         await userClick(action.pid, 'inviteBtn', inviteBtn)
         await sleep(5000)
         const items = document.querySelectorAll('div[aria-checked="false"] i[data-visualcompletion="css-img"]')
+        let count = 0
+        let max = 3
         for (let item of items) {
           await userClick(action.pid, 'item', item)
           await sleep(2000)
+          count++
+          if (count > max) {
+            break
+          }
         }
 
         const confirmBtn = getElementContainsInnerText('span', ['Gửi lời mời'], '', 'equal')
