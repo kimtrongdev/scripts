2b08913ce6909c512947c1c29a0d439f4bde3444
Quees <queesca@gmail.com>
2024-03-02 16:33:52 +0700

    .

--- ex/x_scripts/comment_x.js
+++ ex/x_scripts/comment_x.js
@@ -1,33 +1,30 @@
 async function commentX(action) {
   try {
-    await sleep(3000)
+    await sleep(5000);
+    console.log("commentTwitter action : ", action);
     let url = window.location.toString();
     reportLive(action.pid);
-    console.log("commentTwitter action : ", action);
-    if (url.indexOf(action.link) > -1) {
-      await sleep(5000);
-      const spanElement = document
-        .querySelector(
-          'div[class="public-DraftStyleDefault-block public-DraftStyleDefault-ltr"]'
-        )
-        .querySelector("span");
+    const spanElement = document
+      .querySelector(
+        'div[class="public-DraftStyleDefault-block public-DraftStyleDefault-ltr"]'
+      )
+      .querySelector("span");
 
-      if (spanElement) {
-        await userClick(action.pid, "spanElement", spanElement);
-        await sleep(5000);
-        await userType(action.pid, '', action.comment_content, spanElement)
-        await sleep(3000);
-        const btnReply = document.querySelector(
-          'div[data-testid="tweetButtonInline"]'
-        );
-        if (btnReply) {
-          await userClick(action.pid, "btnReply", btnReply);
-        } 
-      } 
+    if (spanElement) {
+      await userClick(action.pid, "spanElement", spanElement);
+      await sleep(5000);
+      await userType(action.pid, "", action.comment_content, spanElement);
       await sleep(3000);
-      console.log("spanElement", spanElement);
-      // await reportScript(action)
+      const btnReply = document.querySelector(
+        'div[data-testid="tweetButtonInline"]'
+      );
+      if (btnReply) {
+        await userClick(action.pid, "btnReply", btnReply);
+      }
     }
+    await sleep(3000);
+    console.log("spanElement", spanElement);
+    // await reportScript(action)
   } catch (error) {
     console.log("commentX errr", error);
     // await reportScript(action, false);
