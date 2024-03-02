2337e1e9d36fe894a623b222c2fe171a7feb163c
Quees <queesca@gmail.com>
2024-03-02 16:02:15 +0700

    .

--- ex/x_scripts/follow_x.js
+++ ex/x_scripts/follow_x.js
@@ -1,24 +1,21 @@
 async function followX(action) {
-    try {
-      await sleep(5000)
-      console.log("followX followX followX", action);
-      let url = window.location.toString()
-      reportLive(action.pid)
-
-      if (url.indexOf(action.link) > -1) {
-        let followBtn = document.querySelector(`div[aria-label="Follow ${action.user_name}"]`)
-        if (followBtn) {
-          await userClick(action.pid, 'followBtn', followBtn)
-        }
-      }
-      
-      await sleep(5000)
-      await reportScript(action)
-      
-    } catch (error) {
-      console.log(33, error);
-      await sleep(5000)
-      await reportScript(action, false)
+  try {
+    await sleep(5000);
+    console.log("followX followX followX", action);
+    let url = window.location.toString();
+    reportLive(action.pid);
+    let followBtn = document.querySelector(
+      `div[aria-label="Follow ${action.user_name}"]`
+    );
+    if (followBtn) {
+      await userClick(action.pid, "followBtn", followBtn);
     }
+    console.log('followBtn', followBtn);
+    await sleep(5000);
+    await reportScript(action);
+  } catch (error) {
+    console.log(33, error);
+    await sleep(5000);
+    await reportScript(action, false);
   }
-  
+}
