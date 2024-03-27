f942d8a63a99846713d7261ca6336932615e5bd5
quees <queesca@gmail.com>
2024-03-27 09:29:19 +0700

    .

--- main.js
+++ main.js
@@ -678,11 +678,17 @@ async function newRunProfile() {
         }
     }
 }
-// Hàm lấy dữ liệu script cho một profile
+/**
+ * Lấy dữ liệu script cho một profile
+ * @param {string|number} pid - ID của profile
+ * @param {boolean} [isNewProxy=false] - Cờ chỉ định có tải proxy mới hay không
+ * @returns {Promise<Object|undefined>} - Promise trả về đối tượng action hoặc undefined nếu không tìm thấy dữ liệu
+ */
 async function getScriptData(pid, isNewProxy = false) {
     let action = {}
     if (IS_REG_USER) {
         if (systemConfig.unsub_youtube) {
+            // Lấy thông tin profile cho việc hủy đăng ký kênh YouTube
             action = await request_api.getProfileForRegChannel()
             if (action) {
                 action.pid = action.id
@@ -694,6 +700,7 @@ async function getScriptData(pid, isNewProxy = false) {
             }
         }
         else if (systemConfig.is_change_pass) {
+            // Lấy thông tin profile cho việc thay đổi mật khẩu
             action = await request_api.getProfileForRegChannel(pid)
             if (action) {
                 action.pid = action.id
@@ -705,6 +712,7 @@ async function getScriptData(pid, isNewProxy = false) {
             }
         }
         else if (systemConfig.is_reg_account && systemConfig.new_account_type == 'facebook') {
+            // Lấy thông tin profile cho việc đăng ký tài khoản Facebook mới
             action = await request_api.getProfileForRegChannel(pid)
             if (action) {
                 action.pid = utils.randomRanger(100, 400)
@@ -719,10 +727,10 @@ async function getScriptData(pid, isNewProxy = false) {
             }
         }
         else if (systemConfig.is_check_mail_1 && systemConfig.is_check_mail_1 != 'false') {
+            // Lấy profile mới cho việc kiểm tra email
             let newProfile = await request_api.getNewProfile()
             utils.log('newProfile: ', newProfile)
             if (!newProfile.err && newProfile.profile) {
-                // copy main to clone profile
                 let profile = newProfile.profile
                 pid = profile.id
                 action = {
@@ -736,10 +744,10 @@ async function getScriptData(pid, isNewProxy = false) {
             }
         }
         else if (systemConfig.is_reg_ga && systemConfig.is_reg_ga != 'false') {
+            // Lấy profile mới cho việc đăng ký tài khoản Google
             let newProfile = await request_api.getNewProfile()
             utils.log('newProfile: ', newProfile)
             if (!newProfile.err && newProfile.profile) {
-                // copy main to clone profile
                 let profile = newProfile.profile
                 pid = profile.id
                 action = {
@@ -753,11 +761,13 @@ async function getScriptData(pid, isNewProxy = false) {
                 return
             }
         } else if (systemConfig.is_reg_account && systemConfig.is_reg_account != 'false') {
+            // Thiết lập action cho việc đăng ký tài khoản Gmail
             action = {
                 script_code: 'reg_account',
                 account_type: 'gmail'
             }
         } else {
+            // Lấy thông tin profile cho việc đăng ký kênh
             if (ids.length < MAX_PROFILE) {
                 pid = 0
             }
@@ -775,6 +785,7 @@ async function getScriptData(pid, isNewProxy = false) {
             }
         }
     } else {
+        // Lấy script mới cho profile
         action = await request_api.getNewScript(pid)
     }
 
@@ -782,16 +793,15 @@ async function getScriptData(pid, isNewProxy = false) {
         if (useProxy && isNewProxy) {
             let isLoadNewProxy = true
 
-
             if (isLoadNewProxy || action.is_ver_mail_type) {
-                //let newProxy = await request_api.getProxyV4()
+                // Lấy proxy mới cho profile
                 let proxyV6 = await request_api.getProfileProxy(pid, PLAYLIST_ACTION.WATCH, isLoadNewProxy)
 
                 if (proxyV6) {
                     proxy[pid] = proxyV6
                 }
             } else {
-                //execSync(`sudo gsettings set org.gnome.system.proxy mode 'none'`)
+                // Lấy proxy hiện tại của profile
                 let proxyV6 = await request_api.getProfileProxy(pid, PLAYLIST_ACTION.WATCH, isLoadNewProxy)
                 if (proxyV6 && proxyV6.server) {
                     proxy[pid] = proxyV6
@@ -806,11 +816,13 @@ async function getScriptData(pid, isNewProxy = false) {
         utils.log('Script data: ', action)
 
         if (!runnings.some(i => i.pid == pid)) {
+            // Thêm profile vào danh sách đang chạy
             let startTime = Date.now()
             let actionRecord = { pid: pid, start: startTime, lastReport: startTime, browser: true, action: 'watch' }
             runnings.push(actionRecord)
         }
 
+        // Thiết lập các thông tin cho action
         action.id = action.script_code
         action.pid = pid
         action.is_show_ui = IS_SHOW_UI
@@ -829,8 +841,10 @@ async function getScriptData(pid, isNewProxy = false) {
                 action[key] = systemConfig[key]
             }
         });
-        // init action data
+
+        // Khởi tạo dữ liệu cho action
         if (action.mobile_percent === undefined || action.mobile_percent === null) {
+            // Thiết lập các thông số liên quan đến tỷ lệ thiết bị di động, tìm kiếm, quảng cáo, v.v.
             if (systemConfig.total_rounds_for_change_proxy) {
                 totalRoundForChangeProxy = Number(systemConfig.total_rounds_for_change_proxy)
             }
@@ -854,6 +868,7 @@ async function getScriptData(pid, isNewProxy = false) {
             action.total_channel_created = Number(systemConfig.total_channel_created)
 
             if (['youtube_sub', 'watch', 'watch_video', 'comment_youtube', 'like_fb_page', 'like_fb_post', 'like_youtube'].includes(action.id)) {
+                // Thiết lập vị trí kênh cho action
                 let oldUserPosition = usersPosition.find(u => u.pid == action.pid)
                 if (oldUserPosition) {
                     action.channel_position = Number(oldUserPosition.position)
@@ -863,6 +878,7 @@ async function getScriptData(pid, isNewProxy = false) {
             }
 
             if (action.id == 'watch' || action.id == 'watch_video') {
+                // Thiết lập thông tin về playlist cho action xem video
                 action.total_loop_find_ads = systemConfig.total_loop_find_ads || 0
 
                 if (!action.playlist_url) {
